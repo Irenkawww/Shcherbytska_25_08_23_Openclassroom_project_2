5afe614e629e438ef046c186a8f76684e81a2c7b
Irenwww <irenka.www@gmail.com>
2023-09-03 22:45:16 +0100

    started to work on menu page

---
+++ index.js
@@ -0,0 +1,12 @@
+
+      document.addEventListener("DOMContentLoaded", function () {
+        const loaderContainer = document.querySelector(".loader-container");
+
+        setTimeout(function () {
+          loaderContainer.style.opacity = "0";
+          setTimeout(function () {
+            loaderContainer.style.display = "none";
+          }, 300);
+        }, 1000);
+      });
+    
